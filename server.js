0db4964f2b692ed0b2140c2d25c56f52ab20a648
shilpapanoth <shilpap.mec@gmail.com>
2016-11-23 00:25:55 +0530

    server set up

--- server.js
+++ server.js
@@ -5,23 +5,23 @@ var dbPool = require('./dbPool').init
 app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.json())
 
-/*
+
 require('./routes.js')(app)
 app.listen(7000,function() {
   console.log('The app is running on port 7000')
 })
-*/
 
 
-require('./routes.js')(app)
-dbPool(function(err) {
-	if(err) {
-		console.log('Error with db',err)
-	}
-	else {
-		app.listen(7000,function() {
-			console.log('The app is running on port 7000')
- 		})
- 	}	
-})
+
+// require('./routes.js')(app)
+// dbPool(function(err) {
+// 	if(err) {
+// 		console.log('Error with db',err)
+// 	}
+// 	else {
+// 		app.listen(7000,function() {
+// 			console.log('The app is running on port 7000')
+//  		})
+//  	}	
+// })
 
